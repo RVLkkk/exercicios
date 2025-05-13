0082332ca32840ce597886a6b5e40149b5eb05d4
RVLkkk <00001122907333sp@al.educacao.sp.gov.br>
2025-05-13 11:53:32 -0300

    criamos uma funçao mostrar o cadastro do aluno, obter dados do aluno e iniciar o sistema

--- exercicio5.py
+++ exercicio5.py
@@ -1,7 +1,18 @@
 from exercicio4 import calcular_media
 
+alunos = []
+
+def obter_dados_alunos():
+    nome_aluno = input("informe o nome completo do aluno:")
+    email_aluno = input("informe o email do aluno:")
+    serie_aluno = input("informe a serie do aluno:")
+    nota01_aluno = float(input("informe a primeira nota do aluno:"))
+    nota02_aluno = float(input("informe a segunda nota do aluno:"))
+    nota03_aluno = float(input("informe a terceira nota do aluno:"))
+
+    return cadastrar_aluno(nome_aluno, email_aluno, serie_aluno, nota01_aluno, nota02_aluno, nota03_aluno)
+
 def cadastrar_aluno(aluno, email, serie, nota01=0, nota02=0, nota03=0):
-    alunos = []
 
     notas = [nota01, nota02, nota03]
 
@@ -13,7 +24,32 @@ def cadastrar_aluno(aluno, email, serie, nota01=0, nota02=0, nota03=0):
             "media": calcular_media(notas)
 }
     alunos.append(aluno)
-    media = calcular_media()
+
     return aluno
 
-print(cadastrar_aluno("ygor", "reivulgoliso@gmail.com", "2° TB",7, 9,6))        
+def mostrar_dados_alunos(dados_alunos):
+    for aluno in dados_alunos:
+        print(f"Nome do aluno: {aluno["nome"]} | Email do aluno: {aluno["email"]} | Série do aluno {aluno["serie"]} | Notas do aluno{aluno["notas"]} Média do aluno {aluno["media"]}")
+    
+    return
+
+def iniciar_sistema():
+    while True:
+        print("="*80)
+        print("Opçao 1 => mostrar lista de alunos cadastrados.")
+        print("Opçao 2 => cadastrar aluno.")
+        print("Opçao 3 => sair do sistema.")
+        print("="*80)
+        
+        
+        opcao = input("escolha uma das opçoes acima:")
+
+        if opcao == 1:
+            mostrar_dados_alunos(alunos)
+        elif opcao == 2:
+            obter_dados_alunos()
+        else:
+            print("sistema finalizado")
+            break
+
+iniciar_sistema()
