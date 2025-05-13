e12bdd945cea348c6658a352555b7b7ad163dafd
RVLkkk <00001122907333sp@al.educacao.sp.gov.br>
2025-05-13 07:33:10 -0300

    comando para cadastrar aluno e calcular media do aluno

--- exercicio5.py
+++ exercicio5.py
@@ -1,12 +1,19 @@
-def cadastrar_aluno(aluno, email, serie):
+from exercicio4 import calcular_media
+
+def cadastrar_aluno(aluno, email, serie, nota01=0, nota02=0, nota03=0):
     alunos = []
+
+    notas = [nota01, nota02, nota03]
 
     aluno = {
             "aluno": aluno,
             "email": email,
-            "serie": serie
+            "serie": serie,
+            "nota": notas,
+            "media": calcular_media(notas)
 }
     alunos.append(aluno)
+    media = calcular_media()
     return aluno
 
-print(cadastrar_aluno("ygor", "reivulgoliso@gmail.com", "2° TB",7, 9,6))
+print(cadastrar_aluno("ygor", "reivulgoliso@gmail.com", "2° TB",7, 9,6))        
